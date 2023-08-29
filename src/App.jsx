7dec102ebc98ba258ccde95516562beabf8f56ef
mohannaddQA <qa.mohannad@gmail.com>
2023-08-29 06:28:40 +0000

    lab29 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import "./App.scss";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Form from "./components/form";
 import Results from "./components/results";
+import History from "./components/results/history";
 import axios from "axios";
 
-function App() {
-  const [loading, setLoading] = useState(false);
-  const [requestParams, setRequestParams] = useState({
+let response;
+const initialState = {
+  loading: false,
+  requestParams: {
     method: "GET",
     url: "",
     body: "",
-  });
-  const [response, setResponse] = useState("");
-  const [formattedHeaders, setFormattedHeaders] = useState("");
-  const [formattedData, setFormattedData] = useState("");
+  },
+  response: null,
+  formattedHeaders: "",
+  formattedData: "",
+  history: [],
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CALL_API":
+      return {
+        ...state,
+        loading: true,
+        requestParams: action.payload.requestParams,
+      };
+    case "API_SUCCESS":
+      response = action.payload.response;
+      return {
+        ...state,
+        loading: false,
+        response: action.payload.response,
+        formattedHeaders: JSON.stringify(response.headers, null, 2),
+        formattedData: JSON.stringify(response.data, null, 2),
+        history: [
+          ...state.history,
+          {
+            method: action.payload.requestParams.method,
+            url: action.payload.requestParams.url,
+            response: response.data,
+          },
+        ],
+      };
+    case "API_ERROR":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+      };
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    if (response) {
-      console.log(response);
-      const formattedHeaders = JSON.stringify(response.headers, null, 2);
-      const formattedData = JSON.stringify(response.data, null, 2);
-
-      setFormattedHeaders(formattedHeaders);
-      setFormattedData(formattedData);
+    if (state.response) {
+      console.log("response is triggered");
     }
-  }, [response]);
+  }, [state.response]);
 
   async function callApi(requestParams) {
-    setLoading(true);
+    dispatch({ type: "CALL_API", payload: { requestParams } });
     try {
       const requestOptions = {
         method: requestParams.method,
@@ -38,29 +77,31 @@ function App() {
       };
 
       const apiResponse = await axios(requestOptions);
-      setResponse(apiResponse);
+      dispatch({
+        type: "API_SUCCESS",
+        payload: { requestParams, response: apiResponse },
+      });
     } catch (error) {
+      dispatch({ type: "API_ERROR", payload: { error } });
       console.error("Error fetching data:", error);
-    } finally {
-      setLoading(false);
     }
-    setRequestParams(requestParams);
   }
 
   return (
     <React.Fragment>
       <Header />
       <div className="centered-container">
-        Request Method: {requestParams.method} <br />
-        URL: {requestParams.url}
+        Request Method: {state.requestParams.method} <br />
+        URL: {state.requestParams.url}
       </div>
 
       <Form handleApiCall={callApi} />
       <Results
-        loading={loading}
-        formattedHeaders={formattedHeaders}
-        formattedData={formattedData}
+        loading={state.loading}
+        formattedHeaders={state.formattedHeaders}
+        formattedData={state.formattedData}
       />
+      <History history={state.history} />
       <Footer />
     </React.Fragment>
   );
